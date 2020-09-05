aba16f659af171fbc3870a34730a7d1395e6a92a
dancan-sandys <dancan.oruko96@gmail.com>
2020-09-05 12:11:34 +0300

    add a test to check if the user class has been initialised

---
+++ user.py
@@ -0,0 +1,6 @@
+class User:
+    '''
+    class to create a new user
+    '''
+    
+    
---
+++ user_test.py
@@ -0,0 +1,33 @@
+import unittest
+
+from user import User
+
+class Test_user(unittest.TestCase):
+    
+    '''
+    Test class to test if the user class functions
+    
+    Args:
+        Unittest.Testcase:
+            test case class that helps create test cases
+    '''
+    
+    
+    
+    def setUp(self):
+        
+        self.new_user = User("Dancan","Oruko", "Sandys", "Stanford1*")
+        
+    
+    def test_init(self):
+        
+        '''
+        Test if the user class has been initialized
+        '''
+        self.assertEqual(self.new_user.first_name, "Dancan")
+        self.assertEqual(self.new_user.last_name, "Dancan")
+        self.assertEqual(self.new_user.user_name, "Dancan")
+        self.assertEqual(self.new_user.password, "Dancan")
+        
+if __name__ == "__main__":
+    unittest.main()
